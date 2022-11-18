5be0484ab620b002d5ad9f01910a9cd2cae00a8f
Arnob <arnobdam@gmail.com>
2022-11-18 12:13:07 -0500

    started Root function

---
+++ art-museum/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+const Root = () => {
+  return (
+      <h1>Hello from Root</h1>
+  );
+};
