a63adb0df9a4784aa1bc4f19b29d93e268c1e42d
Arnob <arnobdam@gmail.com>
2022-11-18 16:59:52 -0500

    step 2 of phase 2

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -1,16 +1,27 @@
 import harvardArt from "./data/harvardArt";
 import GalleryNavigation from "./components/GalleryNavigation";
+import GalleryView from "./components/GalleryView";
+import { Switch, Route } from "react-router-dom";
+
+
 
 function App() {
 
   // <GalleryNavigation/>
   let galleries = harvardArt.records;
-    console.log(galleries);
+  console.log(galleries);
   // GalleryNavigation({galleries});
-    
+
   return (
-    <GalleryNavigation galleries={galleries}/> 
+    <>
+      <GalleryNavigation galleries={galleries} />
+      <Switch>
+        <Route path="/galleries/:galleryId" component={GalleryView} />
+      </Switch>
+    </>
   );
 }
+
+
 
 export default App;
---
+++ art-museum/src/components/GalleryView/index.js
@@ -0,0 +1,8 @@
+
+const GalleryView = () => {
+    return (
+        <h1>Hello from GalleryView</h1>
+    )
+}
+
+export default GalleryView;
